a9b01974e225221cc8148c45314a1567594487fb
Wojtek <143072061+Ficowsky@users.noreply.github.com>
2024-10-06 00:15:06 +0200

    Project Done

--- WeatherStationSimulator/Program.cs
+++ WeatherStationSimulator/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter the number of days to simulate");
+            int days = int.Parse(Console.ReadLine());
+
+            int[] temperature = new int[days];
+            string[] conditions = { "Sunny", "Rainy", "Cloudy", "Snowy" };
+            string[] weatherConditions = new string[days];
+
+             Random random = new Random();
+
+            for (int i = 0; i < days; i++)
+            {
+                temperature[i] = random.Next(-20, 55);
+                weatherConditions[i] = conditions[random.Next(conditions.Length)];
+            }
+            Console.WriteLine($"Average temperature: {CalculateAverage(temperature)}");
+            Console.ReadKey();
         }
+
+            static double CalculateAverage(int[] temperature)
+            {
+                double sum = 0;
+                for (int i = 0; i < temperature.Length; i++)
+                {
+                    sum += temperature[i];
+                }
+                return sum / temperature.Length;
+
+            } 
     }
 }
